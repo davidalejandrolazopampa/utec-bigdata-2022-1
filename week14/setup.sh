6e1e2c9129f17f28a84a3a3d2c3113f71d605c1f
Richard Yantas <richard@dysopsis.com>
2022-06-21 21:25:43 -0500

    setup update

--- week14/setup.sh
+++ week14/setup.sh
@@ -1,66 +1,60 @@
-#!/bin/bash
+# https://supergloo.com/spark-sql/
+# https://jdbc.postgresql.org/download.html
+# https://spark.apache.org/docs/latest/sql-data-sources-jdbc.html
+# https://kb.objectrocket.com/postgresql/how-to-run-an-sql-file-in-postgres-846
 
-# install postgres server and mongodb
-sudo yum install -y postgresql-server postgresql-jdbc
 
-# upgrade spark python to work with python 3
-sudo yum upgrade -y spark-python
-
-# update spark log4j to be quiet and restart
-sudo cp setup/log4j.properties /usr/lib/spark/conf/
-sudo chmod 644 /usr/lib/spark/conf/log4j.properties
-sudo service spark-master restart
-sudo service spark-worker restart
+# check version postgresqk 
+# sudo -u postgres psql
+# SELECT version();
+# SHOW server_version;
 
 # create initial database
 sudo service postgresql initdb
 
 # setup authentication
-echo "host    cloudera    cloudera     127.0.0.1/32   trust" > /tmp/pg
-#sudo cat /var/lib/pgsql/data/pg_hba.conf >> /tmp/pg
-sudo cat /etc/postgresql/13/main/pg_hba.conf >> /tmp/pg
-sudo cp /etc/postgresql/13/main/pg_hba.conf /etc/postgresql/13/main/pg_hba.conf.DIST
-sudo mv /tmp/pg /etc/postgresql/13/main/pg_hba.conf
+echo "host    sparksql    sparksql     127.0.0.1/32   trust" > /tmp/pg
+sudo cat /var/lib/pgsql/data/pg_hba.conf >> /tmp/pg
+sudo cp /var/lib/pgsql/data/pg_hba.conf /var/lib/pgsql/data/pg_hba.conf.DIST
+sudo mv /tmp/pg /var/lib/pgsql/data/pg_hba.conf
 
-# start postgres server during boot
-sudo chkconfig --level 345 postgresql on
-
-# start postgres server
-sudo service postgresql start
 
 # set permissions so postgres can read csv files
-chmod 755 /home/cloudera/Downloads/big-data-3
+chmod 755 /home/serendipita/Documents/utec/bigdata/utec-bigdata-2022-1/data/csv/clicks/
 
 # create postgres account for cloudera user
-sudo -u postgres psql -c "CREATE USER cloudera"
+sudo -u postgres psql -c "CREATE USER sparksql"
 
 # make cloudera account admin
-sudo -u postgres psql -c "ALTER USER cloudera with superuser"
+sudo -u postgres psql -c "ALTER USER sparksql with superuser"
 
 # create default db for cloudera account
-sudo -u postgres createdb cloudera
+sudo -u postgres createdb sparksql
+
 
 # decompress csv files
-gzip -d *.csv.gz
+gzip -d ../data/csv/clicks/*.csv.gz
 
 # set permissions so postgres can read csv files
-chmod 644 *.csv
+chmod 644 ../data/csv/clicks/*.csv
 
 # create and load tables for hands on
 psql -f setup/init-postgres.sql
 
-# download and install anaconda for pandas, jupyter
-wget http://repo.continuum.io/archive/Anaconda3-4.0.0-Linux-x86_64.sh
-bash Anaconda3-4.0.0-Linux-x86_64.sh
 
 # add postgres jdbc jar to spark classpath
 echo "export SPARK_CLASSPATH=/usr/share/java/postgresql-jdbc-8.4.704.jar" >> ~/.bashrc
 
 # set environment variables to load spark libs in jupyter
-echo "export PYSPARK_DRIVER_PYTHON_OPTS=\"notebook\"" >> ~/.bashrc
-echo "export PYSPARK_DRIVER_PYTHON=jupyter"  >> ~/.bashrc
+#echo "export PYSPARK_DRIVER_PYTHON_OPTS=\"notebook\"" >> ~/.bashrc
+#echo "export PYSPARK_DRIVER_PYTHON=jupyter"  >> ~/.bashrc
 
-cd mongodb
-./setup.sh
-cd ..
+psql sparksql -h 127.0.0.1 -d sparksql -a -f init-postgres.sql
+# ls -l ../data/csv/
 
+# psql sparksql -h 127.0.0.1 -d sparksql -a -f init-postgres.sql
+
+# df2 = sqlsc.read.format("jdbc") \
+#   .option("url", "jdbc:postgresql://localhost/sparksql?user=sparksql") \
+#   .option("dbtable", "adclicks") \
+#   .load()
